4194eda5a876e0d19dfcef22cbaf90941773a58c
neeharikak2021 <68460124+neeharikak2021@users.noreply.github.com>
2020-07-18 14:18:17 +0530

    final file

---
+++ file1.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul 18 14:05:34 2020
+
+@author: HP
+"""
+
+
+#Topic: Test File
+#-----------------------
+#libraries
+
+#list - ordered collection of items, mutable : [square ]
+list1 = [1,2,3,4,5,'a', 'Neeharika', "Kujur",True] #list type of object with data
+list1 #print when through spyder
+type(list1) #type of  object
+print(list1) #print when running complete file 
+
